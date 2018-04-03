911123304ac916a7e9987c24f59ef7989b89d612
Connor Wood <connorwood71@gmail.com>
2018-04-03 15:32:02 +0100

    Removed empty from KaitaiStruct, added in from_bytes, implemented from_file and from_bytes in KaitaiStruct, and added error checking

--- src/kaitai_struct.rs
+++ src/kaitai_struct.rs
@@ -2,8 +2,24 @@ use std;
 use kaitai_stream::KaitaiStream;
 
 pub trait KaitaiStruct {
-    fn empty() -> Self where Self : Sized;
-    fn from_file(path: &str) -> std::result::Result<Self, std::io::Error> where Self : Sized;
-    fn new<S: KaitaiStream>(stream: &mut S, parent: &Option<Box<KaitaiStruct>>, root: &Option<Box<KaitaiStruct>>) -> Self where Self : Sized;
-    fn read<S: KaitaiStream>(&mut self, stream: &mut S, parent: &Option<Box<KaitaiStruct>>, root: &Option<Box<KaitaiStruct>>) where Self : Sized;
+    fn from_file(path: &str) -> std::io::Result<Self> where Self : Sized {
+        let mut f = std::fs::File::open(path)?;
+        Self::new(&mut f, &None, &None)
+    }
+    
+    fn from_bytes(bytes: Vec<u8>) -> std::io::Result<Self> where Self : Sized {
+        let mut b = std::io::Cursor::new(bytes);
+        Self::new(&mut b, &None, &None)
+    }
+    
+    fn new<S: KaitaiStream>(stream: &mut S,
+                            parent: &Option<Box<KaitaiStruct>>,
+                            root: &Option<Box<KaitaiStruct>>)
+                            -> std::io::Result<Self>
+        where Self : Sized;
+    
+    fn read<S: KaitaiStream>(&mut self,
+                             stream: &mut S,
+                             parent: &Option<Box<KaitaiStruct>>,
+                             root: &Option<Box<KaitaiStruct>>) -> std::io::Result<()> where Self : Sized;
 }
