a849136f9008244455a1a6d3430bae375ae5e65a
Oleg Dolgov <Oleg.Dolgov@gmail.com>
2022-07-22 16:23:33 +0800

    new test switch_manual_int_size_else added.

---
+++ tests/switch_manual_int_size_else_tests.rs
@@ -0,0 +1,59 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+#![allow(dead_code)]
+
+use kaitai::*;
+
+mod helpers_tests;
+use helpers_tests::*;
+
+mod switch_manual_int_size_else;
+use switch_manual_int_size_else::*;
+
+#[test]
+fn basic_parse() {
+    let bytes = get_file_as_byte_vec("formats/bin/switch_tlv.bin");
+    let mut reader = BytesReader::new(&bytes);
+
+    let mut test = SwitchManualIntSizeElse::default();
+    {
+        let res = test.read(&mut reader, None, KStructUnit::parent_stack());
+        println!("{:?}", res);
+        assert!(res.is_ok());
+    }
+
+    assert_eq!(4, test.chunks().len());
+
+    assert_eq!(17, test.chunks()[0].code);
+    if let SwitchManualIntSizeElse_Chunk_Body::SwitchManualIntSizeElse_Chunk_ChunkMeta(s) =  test.chunks[0].body.as_ref().unwrap() {
+        assert_eq!("Stuff", s.title);
+        assert_eq!("Me", s.author);
+    } else {
+        panic!("expected enum SwitchManualIntSizeElse_Chunk_ChunkMeta");
+    }
+
+    assert_eq!(34, test.chunks()[1].code);
+    if let SwitchManualIntSizeElse_Chunk_Body::SwitchManualIntSizeElse_Chunk_ChunkDir(s) =  test.chunks[1].body.as_ref().unwrap() {
+        let strings : Vec<String> = vec!["AAAA", "BBBB", "CCCC"].iter().map(|&s| s.to_string() ).collect();
+        assert_eq!(strings, *s.entries());
+    } else {
+        panic!("expected enum SwitchManualIntSizeElse_Chunk_ChunkDir");
+    }
+ 
+    assert_eq!(51, test.chunks()[2].code);
+    if let SwitchManualIntSizeElse_Chunk_Body::SwitchManualIntSizeElse_Chunk_Dummy(s) =  test.chunks[2].body.as_ref().unwrap() {
+        let raw : Vec<u8> = vec![0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80];
+        assert_eq!(raw, *s.rest());
+    } else {
+        panic!("expected enum SwitchManualIntSizeElse_Chunk_Dummy");
+    }
+ 
+    assert_eq!(255, test.chunks()[3].code);
+    if let SwitchManualIntSizeElse_Chunk_Body::SwitchManualIntSizeElse_Chunk_Dummy(s) =  test.chunks[3].body.as_ref().unwrap() {
+        let raw : Vec<u8> = vec![];
+        assert_eq!(raw, *s.rest());
+    } else {
+        panic!("expected enum SwitchManualIntSizeElse_Chunk_Dummy");
+    }
+}
