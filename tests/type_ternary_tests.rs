8560edc4a1cd47c32a6683be37afe4e94ef53931
Oleg Dolgov <Oleg.Dolgov@gmail.com>
2022-07-25 13:53:00 +0800

    new test type_ternary_tests added.

---
+++ tests/type_ternary_tests.rs
@@ -0,0 +1,26 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+#![allow(dead_code)]
+
+use kaitai::*;
+
+mod helpers_tests;
+use helpers_tests::*;
+
+mod type_ternary;
+use type_ternary::*;
+
+#[test]
+fn basic_parse() {
+    let bytes = get_file_as_byte_vec("formats/bin/term_strz.bin");
+    let mut reader = BytesReader::new(&bytes);
+    let mut test = TypeTernary::default();
+    {
+        let res = test.read(&mut reader, None, KStructUnit::parent_stack());
+        println!("{:?}", res);
+        assert!(res.is_ok());
+    }
+
+    assert_eq!(101, test.dif(&mut reader, None, KStructUnit::parent_stack()).unwrap().value)
+}
